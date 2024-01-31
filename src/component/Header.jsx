48c0a6f6cb4e88399a52845f9e8f2af863272a71
deekayhub <deekay843424@gmail.com>
2024-01-31 08:55:23 +0530

    react

---
+++ src/component/Header.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+     <header>
+      <div className="top-header py-1">
+        <div className="container">
+           <p className='text-center m-0'>Refer and earn extra discount <a href="#">Click here...</a></p>        
+        </div>
+      </div>
+      <div className="main-header">
+      <nav className="py-2">
+        <div className="container-fluid">
+        <div className="row align-items-center">
+        <div className="col-md-3">
+         <a className="navbar-brand d-flex gap-3" href="#">
+            <img src="\src\assets\logo.svg" alt="" />
+            <div>
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="150" height="15" viewBox="0 0 153 15" fill="none">
+                  <path d="M7.84582 14.886C6.22126 14.886 4.85357 14.6074 3.74276 14.0502C2.64583 13.48 1.81967 12.67 1.26426 11.6203C0.708855 10.5706 0.431152 9.32006 0.431152 7.86863C0.431152 6.4172 0.722741 5.16664 1.30592 4.11694C1.90298 3.06725 2.74997 2.2573 3.8469 1.68709C4.95771 1.11689 6.29068 0.831787 7.84582 0.831787C8.91498 0.831787 9.84528 0.974338 10.6367 1.25944C11.4282 1.53158 12.1363 1.93332 12.7612 2.46465L11.9905 4.03919C11.5462 3.68929 11.1158 3.41067 10.6992 3.20332C10.2827 2.99597 9.85223 2.84694 9.4079 2.75623C8.96358 2.65255 8.44288 2.60072 7.84582 2.60072C6.16572 2.60072 4.88829 3.06077 4.01352 3.98087C3.13876 4.90097 2.70137 6.20337 2.70137 7.88807C2.70137 9.59868 3.11793 10.9011 3.95104 11.7953C4.79803 12.6895 6.12406 13.1365 7.92913 13.1365C8.55396 13.1365 9.17185 13.0847 9.7828 12.981C10.3937 12.8644 10.9839 12.6959 11.5532 12.4756L11.1366 13.4281V8.93776H7.51258V7.42154H12.9486V13.8947C12.3099 14.1927 11.5254 14.4325 10.5951 14.6139C9.67866 14.7953 8.76224 14.886 7.84582 14.886Z" fill="black"/>
+                  <path d="M14.3618 14.7111L20.9017 1.00674H22.7554L29.3994 14.7111H27.1709L25.3588 10.8233L26.2753 11.3676H17.3818L18.3607 10.8233L16.5487 14.7111H14.3618ZM21.7973 3.22276L18.6315 10.2013L18.1316 9.75419H25.5255L25.1089 10.2013L21.8806 3.22276H21.7973Z" fill="black"/>
+                  <path d="M30.9758 14.7111V1.00674H37.1825C38.7515 1.00674 39.9665 1.35663 40.8273 2.05643C41.6882 2.74327 42.1187 3.70873 42.1187 4.95281C42.1187 5.76924 41.9243 6.46904 41.5355 7.0522C41.1467 7.63537 40.5982 8.08246 39.8901 8.39348C39.1819 8.69154 38.3488 8.84057 37.3908 8.84057L37.724 8.52955H38.2864C38.8556 8.52955 39.3625 8.66562 39.8068 8.93776C40.265 9.20991 40.6399 9.64404 40.9315 10.2402L43.0351 14.7111H40.6607L38.5779 10.2207C38.3697 9.6894 38.0434 9.34598 37.599 9.19047C37.1686 9.03496 36.634 8.9572 35.9953 8.9572H33.1419V14.7111H30.9758ZM33.1419 7.42154H36.8284C37.8698 7.42154 38.6543 7.21419 39.1819 6.7995C39.7235 6.3848 39.9942 5.78868 39.9942 5.01113C39.9942 4.22062 39.7235 3.62449 39.1819 3.22276C38.6543 2.82102 37.8698 2.62016 36.8284 2.62016H33.1419V7.42154Z" fill="black"/>
+                  <path d="M45.1338 14.7111V1.00674H46.8L55.8184 12.048H55.2977V1.00674H57.318V14.7111H55.6726L46.675 3.66985H47.1541V14.7111H45.1338Z" fill="black"/>
+                  <path d="M60.3901 14.7111V1.00674H70.0125V2.62016H62.4937V6.93557H69.5335V8.56843H62.4937V13.0782H70.0125V14.7111H60.3901Z" fill="black"/>
+                  <path d="M75.6679 14.7111V2.69791H70.6484V1.00674H82.8534V2.69791H77.834V14.7111H75.6679Z" fill="black"/>
+                  <path d="M89.7043 14.7111V1.00674H91.8704V12.981H99.0768V14.7111H89.7043Z" fill="black"/>
+                  <path d="M98.9248 14.7111L105.465 1.00674H107.318L113.962 14.7111H111.734L109.922 10.8233L110.838 11.3676H101.945L102.924 10.8233L101.112 14.7111H98.9248ZM106.36 3.22276L103.194 10.2013L102.695 9.75419H110.088L109.672 10.2013L106.444 3.22276H106.36Z" fill="black"/>
+                  <path d="M115.539 14.7111V1.00674H117.205L126.223 12.048H125.703V1.00674H127.723V14.7111H126.078L117.08 3.66985H117.559V14.7111H115.539Z" fill="black"/>
+                  <path d="M130.795 14.7111V1.00674H140.418V2.62016H132.899V6.93557H139.938V8.56843H132.899V13.0782H140.418V14.7111H130.795Z" fill="black"/>
+                  <path d="M142.736 14.7111V1.00674H152.358V2.62016H144.84V6.93557H151.879V8.56843H144.84V13.0782H152.358V14.7111H142.736Z" fill="black"/>
+                </svg>
+                </div>
+                <small>Dazzling you Today, Tomorrow & Forever</small>
+            </div>
+         </a>
+         </div>
+          
+          <div className="col-md-3">
+          <form className="d-flex position-relative " role="search">
+              <input className="form-control rounded-pill search-input" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn position-absolute end-0" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+          </div>
+          <div className="col-md-6 align-center header-menu">
+            <div className="d-flex justify-content-between align-items-center">
+                <a href="#" className="text-center">
+                    <img src="\src\assets\offers.svg" alt="" />
+                    <p className='text-uppercase'>Offers</p>
+                </a>             
+                <a href="#" className="text-center">
+                    <img src="\src\assets\contact-us.svg" alt="" />
+                    <p className='text-uppercase'>Contact Us</p>
+                </a>             
+                <a href="#" className="text-center">
+                    <img src="\src\assets\wishlist.svg" alt="" />
+                    <p className='text-uppercase'>Wishlist</p>
+                </a>             
+                <a href="#" className="text-center">
+                    <img src="\src\assets\delivery.svg" alt="" />
+                    <p className='text-uppercase'>Delivery</p>
+                </a>             
+                <a href="#" className="text-center">
+                    <img src="\src\assets\cart.svg" alt="" />
+                    <p className='text-uppercase'>Cart</p>
+                </a>             
+                <a href="#" className="text-center">
+                    <img src="\src\assets\verify-report.svg" alt="" />
+                    <p className='text-uppercase'>Verify Report</p>
+                </a>             
+                 
+                <a href="#" className="profile border-bottom">
+                    <span>Hi, Ashish Sood</span>
+                </a>             
+            </div>
+          </div>
+        </div>
+        </div>
+      </nav>
+      </div>
+    </header>
+    <nav className='container-fluid border menu-item'>
+    <div class="list-group d-flex flex-row justify-content-between">       
+        <a href="#" className="list-group-item border-0 bg-transparent">NEW ARRIVAL</a>
+        <a href="#" className="list-group-item border-0 bg-transparent">RINGS</a>
+        <a href="#" className="list-group-item border-0 bg-transparent">EARRINGS</a>
+        <a href="#" className="list-group-item border-0 bg-transparent">PENDANTS</a>
+        <a href="#" className="list-group-item border-0 bg-transparent">BRACELETS & BANGLES</a>
+        <a href="#" className="list-group-item border-0 bg-transparent">SOLITIARES</a>
+        <a href="#" className="list-group-item border-0 bg-transparent">GOLD COINS</a>
+        <a href="#" className="list-group-item border-0 bg-transparent">ALL JEWELLERY</a>
+      </div>
+    </nav>
+    <div className="container-fluid">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li className="breadcrumb-item"><a href="#">Home</a></li>
+          <li className="breadcrumb-item"><a href="#">Product</a></li>
+          <li className="breadcrumb-item"><a href="#">Earings</a></li>
+          <li className="breadcrumb-item active" aria-current="page">Just Lovely Earings</li>
+        </ol>
+      </nav>
+    </div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-5"></div>
+        <div className="col-md-7">
+          <div className="bg-light rounded p-1">
+           <div className="d-flex justify-content-between">
+           <div className="product-title">
+              <h5>Dainty Crown Diamond Band Ring</h5>
+              <span>Floral shine that can Entice</span>
+            </div>
+            <div className="icon">
+              <span className='pe-2'><i className="bi bi-heart"></i></span>
+              <span><i className="bi bi-share"></i></span>
+            </div>
+           </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Header;  
